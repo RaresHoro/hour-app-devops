9f32cf90e666ef9be816276063ea99baf96f5787
ENDAVA\aulican <Alexandru.Ulican@endava.com>
2025-06-25 13:20:14 +0300

    Test branch commit

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t hour-app:main .
-docker run -p 5000:5000 -e APP_VERSION=dev hour-app:dev
+docker build -t hour-app:test .
+docker run -p 5000:5000 -e APP_VERSION=dev hour-app:test
