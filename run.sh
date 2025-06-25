429aa8dab68bbef16057fdd5f76304d2c3a2aeef
RaresHoro <horodinca.rares@yahoo.com>
2025-06-25 14:25:46 +0300

    Test branch

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t hour-app:main .
-docker run -p 5000:5000 -e APP_VERSION=main hour-app:main
+docker build -t hour-app:test .
+docker run -p 5000:5000 -e APP_VERSION=test hour-app:test
